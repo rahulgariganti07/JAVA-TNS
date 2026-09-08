07fed8a3d1ddc7a3913529c1a6f07b1ee66c49ab
rahul <rahul@LAPTOP-7RVU2R77>
2026-09-08 22:41:18 +0530

    jagged array

---
+++ src/java_learnings_package/JaggedArray.java
@@ -0,0 +1,43 @@
+package java_learnings_package;
+
+public class JaggedArray {
+
+	public static void main(String[] args) {
+		int[][] arr = new int[3][];
+		
+		arr[0] = new int[3];
+		arr[1] = new int[4];
+		arr[2] = new int[2];
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				arr[i][j]=(int)(Math.random()*10);
+			}
+		}
+		
+		/*for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}*/
+		
+		//Enhanced forloop
+		
+		for(int n[] : arr)
+		{
+			for(int m: n)
+			{
+				System.out.print(m+" ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
