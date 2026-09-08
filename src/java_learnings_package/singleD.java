1fc0dce8386e86d39928a176e63320a517547c12
rahul <rahul@LAPTOP-7RVU2R77>
2026-09-08 22:37:31 +0530

    1d array

---
+++ src/java_learnings_package/singleD.java
@@ -0,0 +1,22 @@
+package java_learnings_package;
+
+public class singleD {
+
+	public static void main(String[] args) {
+		int[] nums = new int[5];
+		
+		nums[0]=10;
+		nums[1]=20;
+		nums[2]=30;
+		nums[3]=40;
+		nums[4]=50;
+	
+		
+	for(int i=0;i<=4;i++)
+	{
+		System.out.println(nums[i]);
+	}
+	}
+	
+
+}
